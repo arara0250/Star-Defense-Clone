f8f645f15547edec3200ef5957d55b4d985c6029
arara0250 <arara4281@gmail.com>
2026-08-02 15:07:09 +0900

    feat : 블록에 영웅 소환 시, 중복 검사 로직 추가 (HeroSpawner.cs)

--- Assets/01_Scripts/HeroSpawner.cs
+++ Assets/01_Scripts/HeroSpawner.cs
@@ -6,7 +6,13 @@ public class HeroSpawner : MonoBehaviour
 
     public void TrySpawnHero(Transform blockTransform)
     {
+        // 블록의 영웅 소환 여부 검사 (중복 방지)
+        Block block = blockTransform.GetComponent<Block>();
+        if ( block.IsSpawnHero )
+            return;
+
         // 선택한 위치의 블록에 영웅 소환
         Instantiate(heroPrefab, blockTransform.position, Quaternion.identity);
+        block.IsSpawnHero = true;
     }
 }
