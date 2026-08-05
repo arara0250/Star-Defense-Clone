9e2a3a5715e2f63212ce5199f949b111c2d1a3ee
arara0250 <arara4281@gmail.com>
2026-08-05 10:37:14 +0900

    feat : 웨이브 시스템 구현
    1. WaveSystem.cs 스크립트 작성
      - Wave 구조체 설계
      - 웨이브 진행 로직 작성
    
    2. WaveSystem 오브젝트 생성
      - Inpector 뷰에서 웨이브 정보 입력 (웨이브 시간, 적 프리팹 등)

---
+++ Assets/01_Scripts/WaveSystem.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct Wave
+{
+    public float        waveTime;       // 현재 웨이브에서 일반 적을 생성하는 시간(초)
+    public GameObject[] enemyPrefabs;   // 현재 웨이브에서 등장하는 적의 종류
+    public GameObject   bossPrefab;     // 현재 웨이브의 마지막에 등장할 보스 프리팹
+}
+
+public class WaveSystem : MonoBehaviour
+{
+    [SerializeField] private Wave[]         waves;          // 현재 스테이지의 모든 웨이브
+    [SerializeField] private EnemySpawner   enemySpawner;   // 적 스폰 메소드 호출을 위해 필요
+
+    private int currentWaveIndex = -1;
+
+    public void TryStartNextWave()
+    {
+        // 현재 웨이브 적(일반+보스) 스폰이 완료되었고 && 모든 적을 처치하였고 && 웨이브가 남아있으면 다음 웨이브 시작
+        if ( enemySpawner.IsSpawnFinished 
+          && enemySpawner.EnemyList.Count == 0 
+          && currentWaveIndex < waves.Length - 1 )
+        {
+            currentWaveIndex++;
+            enemySpawner.StartWave(waves[currentWaveIndex]);    // 현재 웨이브 정보를 매개변수로 전달
+        }
+    }
+}
